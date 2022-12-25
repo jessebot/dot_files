d3e7450af6cbae788fefb5ffdf175183a219413d
Jesse Hitch <jessebot@linux.com>
2022-12-25 23:07:35 +0100

    default wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+
+return {
+       hide_tab_bar_if_only_one_tab = true,
+       window_decorations = "RESIZE",
+       -- default terminal colors
+       colors = {
+           ansi = {
+               '#282727',
+               '#fe8ea2',
+               '#97e178',
+               '#f6f76a',
+               '#6eb3ec',
+               '#ef8ef6',
+               '#86f0e4',
+               '#dadfe0',
+           },
+           brights = {
+               '#282727',
+               '#fe8ea2',
+               '#97e178',
+               '#f6f76a',
+               '#6eb3ec',
+               '#ef8ef6',
+               '#86f0e4',
+               '#dadfe0',
+           },
+       },
+}
