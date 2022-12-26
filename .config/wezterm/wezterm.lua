555ecd10f2f8ccb996e19b55998fb41a4239d2b2
Jesse Hitch <jessebot@linux.com>
2022-12-26 22:31:54 +0000

    fixing wezterm fontsize

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
-       font_size = 15,
+       font_size = 12,
        hide_tab_bar_if_only_one_tab = true,
        window_decorations = "RESIZE",
        -- default terminal colors
