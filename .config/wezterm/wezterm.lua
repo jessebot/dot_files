bcd6bdd7be3d39e13b1ff018e9a6de07563240eb
Jesse Hitch <jesse@overstory.com>
2023-03-02 21:01:50 +0100

    updating wezterm config for new resolution and prettier colors for background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,14 +3,16 @@ local wezterm = require 'wezterm'
 return {
        warn_about_missing_glyphs = false,
        audible_bell = "Disabled",
-       initial_cols = 120,
-       initial_rows = 36,
-       font_size = 12,
+       initial_cols = 162,
+       initial_rows = 50,
+       font_size = 15,
        hide_tab_bar_if_only_one_tab = true,
        window_decorations = "RESIZE",
-       window_background_opacity = 0.8,
+       window_background_opacity = 0.95,
        -- default terminal colors
        colors = {
+           background = '#232336',
+           foreground = '#c0caf5',
            ansi = {
                '#282727',
                '#fe8ea2',
