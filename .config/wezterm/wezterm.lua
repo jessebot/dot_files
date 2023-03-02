462deb97c833b7904c67d375f2ae84d86e5b32c6
Jesse Hitch <jesse@overstory.com>
2023-03-02 21:53:57 +0100

    adding wezterm colors for cursor and split

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,6 +13,27 @@ return {
        colors = {
            background = '#232336',
            foreground = '#c0caf5',
+
+           -- Overrides the cell background color when the current cell is occupied by the
+           -- cursor and the cursor style is set to Block
+           cursor_bg = '#97e178',
+           -- Overrides the text color when the current cell is occupied by the cursor
+           cursor_fg = 'black',
+           -- Specifies the border color of the cursor when the cursor style is set to Block,
+           -- or the color of the vertical or horizontal bar when the cursor style is set to
+           -- Bar or Underline.
+           cursor_border = '#6eb3ec',
+
+           -- the foreground color of selected text
+           selection_fg = 'black',
+           -- the background color of selected text
+           selection_bg = '#f6f76a',
+
+           -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+           scrollbar_thumb = '#222222',
+
+           -- The color of the split lines between panes
+           split = '#444444',
            ansi = {
                '#282727',
                '#fe8ea2',
