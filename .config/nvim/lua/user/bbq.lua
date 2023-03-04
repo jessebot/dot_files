6e44aad4d99e47e3f33bb601ee349c07e87dfa5e
Jesse Hitch <jesse@overstory.com>
2023-03-04 10:28:32 +0100

    trying out different colors for bbq

--- .config/nvim/lua/user/bbq.lua
+++ .config/nvim/lua/user/bbq.lua
@@ -1,77 +1,45 @@
--- bluegrays from tokyonight that are not in use
---       "#3d59a1"
---       "#292e42"
---       "#414868"
---       "#c0caf5"
---       "#a9b1d6"
---       "#3b4261"
---       "#545c7e"
---       "#565f89"
---       "#737aa2"
-
 require("barbecue").setup({
   theme = {
       normal = { bg = "NONE", fg = "#bdd8ff" },
 
       ellipsis = { fg = "#585858" },
       separator = { fg = "#585858" },
+      context_operator = { fg = "#fdcd36" },
+
       modified = { fg = "#ff8d87" },
 
-      dirname = { fg = "#585858" },
-      basename = { fg = "#bdd8ff", bold = true },
       context = { fg = "#bdd8ff" },
-
+      basename = { fg = "#bdd8ff", bold = true },
       context_file = { fg = "#bdd8ff" },
-      context_module = { fg = "#2ac3de" },
+      dirname = { fg = "#bdd8ff" },
+
+      context_null = { fg = "#585858" },
+
+      context_string = { fg = "#a8fd57" },
+      context_number = { fg = "#f7fb53" },
+      context_boolean = { fg = "#fdcd36" },
+      context_type_parameter = { fg = "#C1FF87" },
+
+      context_variable = { fg = "#2569aa" },
+      context_constant = { fg = "#2ac3de" },
+      context_property = { fg = "#5cc9fd" },
+
+      context_module = { fg = "#5f87ff" },
+      context_class = { fg = "#7aa2f7" },
+      context_function = { fg = "#3d59a1" },
+      context_method = { fg = "#6DF2E5" },
+
       context_namespace = { fg = "#f7fb53" },
-      context_package = { fg = "#5cc9fd" },
-      context_class = { fg = "#fdcd36" },
-      context_method = { fg = "#5cc9fd" },
-      context_property = { fg = "#C1FF87" },
-      context_field = { fg = "#C1FF87" },
-      context_constructor = { fg = "#5cc9fd" },
+      context_package = { fg = "#737aa2" },
+      context_field = { fg = "#a3a8f8" },
+      context_constructor = { fg = "#565f89" },
       context_enum = { fg = "#f289f9" },
       context_interface = { fg = "#fdcd36" },
-      context_function = { fg = "#5cc9fd" },
-      context_variable = { fg = "#ffaff9" },
-      context_constant = { fg = "#ffaff9" },
-      context_string = { fg = "#a8fd57" },
-      context_number = { fg = "#fdcd36" },
-      context_boolean = { fg = "#6DF2E5" },
       context_array = { fg = "#7dcfff" },
-      context_object = { fg = "#fdcd36" },
-      context_key = { fg = "#5f87ff" },
-      context_null = { fg = "#2ac3de" },
-      context_enum_member = { fg = "#C1FF87" },
+      context_object = { fg = "#d092fc" },
+      context_key = { fg = "#5cc9fd" },
+      context_enum_member = { fg = "#a8fd57 " },
       context_struct = { fg = "#d092fc" },
       context_event = { fg = "#ff8d87" },
-      context_operator = { fg = "#fdcd36" },
-      context_type_parameter = { fg = "#C1FF87" },
     }
 })
---        #E8FBFF bright white
---        #bdd8ff off blueish white
---        #ffaff9 light pink
---        #f289f9 neon magenta
---        #d092fc        ------------------- not in use
---        #f2748a pale dark red
---        #ff8d87 soft redish orange
---        #fdcd36 light orange
---        #f7fb53 bright yellow
---        #f6f76a soft yellow
---        #C1FF87 soft green
---        #a8fd57 lime green
---        #6DF2E5 cyan
---        #2ac3de dark cyan
---        #5cc9fd blue
---        #7dcfff", ----------------- from tokyonight
---        #5f87ff cornflower blue
---        #7aa2f7", ----------------- from tokyonight
---        #a3a8f8 medium purple
---
---        #232336 blueish black
---        #1d2652 navy blue
---        #2569aa darker blue
---        #585858 gray
---        #3E3E3E darker gray
---        #323232 grayish black
