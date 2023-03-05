a7d2d02b3075058433fc8c468c7e6840e3deb5ef
Jesse Hitch <jessebot@linux.com>
2023-03-05 15:26:53 +0100

    ensure my most common languages are installed for treesitter, including experimental markdown/html stuff

--- .config/nvim/lua/user/tree-sitter.lua
+++ .config/nvim/lua/user/tree-sitter.lua
@@ -1,9 +1,24 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  -- ensure_installed = { "lua", "yaml", "bash", "hcl", "python", "kdl", "toml" },
+  ensure_installed = {
+                      "bash",
+                      "css",
+                      "dockerfile",
+                      "hcl",
+                      "html",
+                      "kdl",
+                      "lua",
+                      "markdown",
+                      "markdown_inline",
+                      "python",
+                      "sql",
+                      "terraform",
+                      "toml",
+                      "yaml",
+                      },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  -- sync_install = true,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
