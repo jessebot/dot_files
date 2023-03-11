6d3f5e65ebd15566f25a870445e2279cb6738f6a
Jesse Hitch <jessebot@linux.com>
2023-03-11 10:27:02 +0100

    adding gitui and k9s as floating terminal apps mapped to ALT+g and ALT+k respectively

--- .config/nvim/lua/user/fterm.lua
+++ .config/nvim/lua/user/fterm.lua
@@ -1,8 +1,39 @@
--- ------------------------ floating windows! --------------------------
+-- this file is for customizations for floating terminals in neovim
 
--- open a floating window
-vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
--- close a floating window
-vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
--- or create a vim command
-vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
+local fterm = require("FTerm")
+
+-- --------------- open gitui (must be installed beforehand) -----------------
+local gitui = fterm:new({
+    ft = 'fterm_gitui',
+    cmd = "gitui",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+-- Use this to toggle gitui in a floating terminal
+-- firt arg of 'n' is for normal mode
+-- <A-g> are keys: Alt + g (alt is option on macOS)
+vim.keymap.set('n', '<A-g>', function()
+    gitui:toggle()
+end)
+
+
+-- --------------- open k9s (must be installed beforehand) -----------------
+
+local k9s = fterm:new({
+    ft = 'fterm_k9s',
+    cmd = "k9s",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+-- Use this to toggle gitui in a floating terminal
+-- firt arg of 'n' is for normal mode
+-- <A-k> are keys: Alt + k (alt is option on macOS)
+vim.keymap.set('n', '<A-k>', function()
+    k9s:toggle()
+end)
