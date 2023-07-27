e20fdf0e8a2764bf96a83c233f49680915cb3420
jessebot <jessebot@linux.com>
2023-07-27 15:04:10 +0200

    add gotmpl for tree sitter

--- .config/nvim/lua/user/tree-sitter.lua
+++ .config/nvim/lua/user/tree-sitter.lua
@@ -45,3 +45,13 @@ require'nvim-treesitter.configs'.setup {
   -- checkout incremental selection:
   -- https://github.com/LazyVim/LazyVim/blob/7a8ca6222a554bdb78fb8de35404672fc4042302/lua/lazyvim/plugins/treesitter.lua#L57
 }
+
+local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = {"src/parser.c"}
+  },
+  filetype = "gotmpl",
+  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+}
