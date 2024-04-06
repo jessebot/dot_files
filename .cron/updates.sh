2e3d5e2935f6f77689b25fad011ec69591533036
jessebot <jessebot@linux.com>
2024-04-06 09:17:53 +0200

    run onboardme instead of brew update and upgrade, and always overwrite files

--- .cron/updates.sh
+++ .cron/updates.sh
@@ -1,27 +1,25 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: updates.sh
-#
-#         USAGE: ./updates.sh
-#
-#   DESCRIPTION: Run updates for packages managers and various programs.
-#                Currently Supported programs: tldr, brew
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#         NOTES: ---
-#        AUTHOR: @jessebot 
-#       CREATED: 09/16/2022 12:31:37 PM
-# LAST_MODIFIED: 2022-09-27 07:24:04.0 +0000
-#===============================================================================
+#==============================================================================#
+#                                                                              #
+#          FILE: updates.sh                                                    #
+#                                                                              #
+#         USAGE: ./updates.sh                                                  #
+#                                                                              #
+#   DESCRIPTION: Run updates for packages managers and various programs.       #
+#                Currently Supported programs: tldr, brew                      #
+#                                                                              #
+#       OPTIONS: ---                                                           #
+#  REQUIREMENTS: ---                                                           #
+#        AUTHOR: @jessebot                                                     #
+#       CREATED: 09/16/2022 12:31:37 PM                                        #
+# LAST_MODIFIED: Sat Apr  6 09:08:58 AM CEST 2024                              #
+#==============================================================================#
 
-set -o nounset                                  # Treat unset variables as an error
+# Treat unset variables as an error
+set -o nounset
 
 # update my tldr data (cli tool for cheatsheets)
 tldr --update
 
-# update brew cache and installer
-brew update
-# upgrade all brew packages every evening
-brew upgrade
+# updates all packages
+onboardme -O
