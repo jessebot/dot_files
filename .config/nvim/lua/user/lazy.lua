de4807e7f9dd612443eb4a349ffdc439d5839ccd
Max Roby <max@lil-mac-5.local>
2025-01-01 12:52:14 +0100

    add markdown live preview addon for neovim

--- .config/nvim/lua/user/lazy.lua
+++ .config/nvim/lua/user/lazy.lua
@@ -427,6 +427,22 @@ local plugins = {
         'nvim-tree/nvim-web-devicons',
         lazy = true
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      build = "cd app && yarn install",
+      init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+    }
+
 }
 
 require("lazy").setup(plugins)
