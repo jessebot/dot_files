f10b11f6e685f7ecc2f2d5b6213026487bb3e6eb
jessebot <jessebot@linux.com>
2025-08-20 19:55:00 +0200

    fix spacing in lua/user/lsp-configs.lua

--- .config/nvim/lua/user/lsp-configs.lua
+++ .config/nvim/lua/user/lsp-configs.lua
@@ -141,38 +141,38 @@ lspconfig.vimls.setup{
 -- yaml
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#yamlls
 lspconfig.yamlls.setup {
-  settings = {
-    yaml = {
-      schemas = {
-        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.3-standalone-strict/all.json"] = "/manifests/*",
-        ["https://json.schemastore.org/github-workflow.json"] = "*/.github/workflows/*",
-        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "/*argocd_app.yaml",
-        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/applicationset_v1alpha1.json"] = "/*argocd_appset.yaml",
-        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/appproject_v1alpha1.json"] = "/*argocd_project.yaml",
-        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/external-secrets.io/externalsecret_v1beta1.json"] = "/external_secrets/*",
-        [""] = ""
-            },
-    }},
-    capabilities = capabilities
+    settings = {
+        yaml = {
+          schemas = {
+            ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.3-standalone-strict/all.json"] = "/manifests/*",
+            ["https://json.schemastore.org/github-workflow.json"] = "*/.github/workflows/*",
+            ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "/*argocd_app.yaml",
+            ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/applicationset_v1alpha1.json"] = "/*argocd_appset.yaml",
+            ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/appproject_v1alpha1.json"] = "/*argocd_project.yaml",
+            ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/external-secrets.io/externalsecret_v1beta1.json"] = "/external_secrets/*",
+            [""] = ""
+                },
+        }},
+        capabilities = capabilities
 }
 
 vim.diagnostic.config({
-  virtual_text = false,
+    virtual_text = false,
     signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = ' ',
-            [vim.diagnostic.severity.WARN] = ' ',
-            [vim.diagnostic.severity.INFO] = ' ',
-            [vim.diagnostic.severity.HINT] = ' ',
-        },
-        linehl = {
-            [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-        },
-        numhl = {
-            [vim.diagnostic.severity.WARN] = 'WarningMsg',
-        },
-  },
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
+          text = {
+              [vim.diagnostic.severity.ERROR] = ' ',
+              [vim.diagnostic.severity.WARN] = ' ',
+              [vim.diagnostic.severity.INFO] = ' ',
+              [vim.diagnostic.severity.HINT] = ' ',
+          },
+          linehl = {
+              [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+          },
+          numhl = {
+              [vim.diagnostic.severity.WARN] = 'WarningMsg',
+          },
+    },
+    underline = true,
+    update_in_insert = false,
+    severity_sort = false,
 })
